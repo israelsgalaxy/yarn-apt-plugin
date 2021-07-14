85497d6ee1edd62cac4796ebf29fad464a0bea67
Ajayi Olatunji <ajayiolatunji15@gmail.com>
2021-07-14 15:57:07 +0100

    include old packages from /usr/lib/nodejs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { writeFileSync } from "fs";
 import readdirp from "readdirp"
 
 module.exports = {
@@ -6,87 +7,44 @@ module.exports = {
          
 
         const {Command, UsageError,} = require(`clipanion`);
-        const nodeDir = '/usr/share/nodejs'
+        const nodeDir = '/usr/share/nodejs';
+        const oldNodeDir = '/usr/lib/nodejs';
         class Ceck extends Command {
-            async execute(){               
-                //lists names of packages
-                // const getDirectories = (source: any) => readdirSync(source, {withFileTypes: true})
-                // .filter(dirent => dirent.isDirectory())
-                // .map(dirent => dirent.name)
-                
-                // const modules = getDirectories(nSodeDir)
-                // const atModule = modules.filter((module) => module.startsWith('@'))
+            async execute(){  
                 
                 const moduleDir = async (source: any) => {
                     var paths: string[] = [];
                     for await (const iterator of readdirp(source, {
-                        fileFilter: ['*.js', 'index.js', '!*.test.js'],
+                        // fileFilter: ['*.js', 'index.js', '!*.test.js'],
                         directoryFilter: ['!node_modules', '!types'],
-                        depth: 4})) {
+                        type: 'directories',
+                        depth: 1})) {
                        paths.push((iterator.fullPath))                       
                     }
-                    // const path = JSON.stringify(paths)
-                    // writeFileSync('./paah.txt', path)
                     return paths;
                    
                 } 
-                const themodule = await moduleDir(nodeDir);
-                // console.log(themodule);
+                const nodeModule = await moduleDir(nodeDir);
+                const oldNodeModule = await moduleDir(oldNodeDir);
+                const allModules = [...nodeModule, ...oldNodeModule]
+                // const merged = [].concat.apply([], allModules);
+                
                 const regex = new RegExp(`\\b/${this.package}\\b`)
-                const matcch = themodule.filter(value => regex.test(value))
+                const matcch = allModules.filter(value => regex.test(value))
                 if (matcch.length > 0) {
                         console.log(matcch);
                 } else {
                     this.context.stdout.write(`Module is not installed\n`)
                 }
                 
-                
-                // //if package isn't found in first dir level, check next
-                // if (modules.indexOf(this.package) == -1) {  
-                //     // const innerModule = atModule.map((modul) =>
-                //     //  getDirectories(join(nodeDir, modul)))
-                //     // .reduce((prev, curr) => prev.concat(curr));
-                    
-                //     // if (innerModule.indexOf(this.package) == -1) {
-                //     //     this.context.stdout.write(`don't exist`)
-                //     // }
-                //     var paths: string[] = [];
-                //     for await (const iterator of readdirp(nodeDir, {fileFilter: 'index.js'})) {
-                //        paths.push((iterator.fullPath))                       
-                //     }
-                //     const path = JSON.stringify(paths)
-                //     writeFileSync('./paah.txt', path)
-                    
-                //     //find the real path of package
-                //     // this.context.stdout.write(`\n${realpathSync(`${this.package}`)}\n`);
-                // } else{
-                // this.context.stdout.write(`${this.package}\n`);
-                // }
+              
             }
         }
 
-        // class AllPkg extends Command {
-        //     async execute() {
-        //         // this.context.stdout.write(`Hey \n`);
-        //         const checkcwd = this.context.cwd;
-        //         const getFiles = (source: any) => readdirSync(source, {withFileTypes: true})
-        //         .filter(dirent => dirent.isFile())
-        //         .map(dirent => dirent.name)
 
-        //         const file_ = getFiles(checkcwd);
-        //         if (file_.indexOf('package.json') == -1) {    
-        //             throw new UsageError(`You are working in a wrong directory\n`)                
-        //         } else{
-        //         this.context.stdout.write(`${this.package}\n`);
-        //         }
-                
-        //     }
-        // }
         Ceck.addOption(`package`, Command.String(`--resolve`))
         Ceck.addOption(`package`, Command.String(`-r`))
         Ceck.addPath(`dop-check`);
-
-        // AllPkg.addPath(`dop-install`);
 
         Ceck.usage = Command.Usage({
             description: `
@@ -110,7 +68,6 @@ module.exports = {
         return{
             commands: [
                 Ceck,
-                // AllPkg
             ]
         }
     }
