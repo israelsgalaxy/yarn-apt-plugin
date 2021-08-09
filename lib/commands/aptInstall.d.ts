a8ef038bcc6fc04b055ba829c388d65afc0434d7
Nkwuda Sunday Cletus <scletus40@yahoo.com>
2021-08-09 20:31:44 +0100

    resolve runtime deps by default and devDeps with --dev option

--- lib/commands/aptInstall.d.ts
+++ lib/commands/aptInstall.d.ts
@@ -4,6 +4,7 @@ export default class AptCommand extends BaseCommand {
     static paths: string[][];
     static usage: Usage;
     localOnly: boolean;
+    dev: boolean;
     execute(): Promise<number>;
     find(pkg_names: string[]): Promise<{
         found: {
